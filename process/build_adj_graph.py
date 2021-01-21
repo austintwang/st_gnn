97ac79602bee08cdf7ebd2d48faf482995aac314
Austin Wang <austinwang@Austins-MBP>
2021-01-21 14:04:10 -0800

    spatial graph construction

--- process/build_adj_graph.py
+++ process/build_adj_graph.py
@@ -114,9 +114,9 @@ def build_graphs(params, in_paths, out_dir):
 
 if __name__ == '__main__':
     params = [
-        (10, 20),
-        (100, 200),
-        (1000, 2000)
+        (10, 100),
+        (100, 1000),
+        (1000, 10000)
     ]
 
     data_path = "/dfs/user/atwang/data/spt_zhuang/"
