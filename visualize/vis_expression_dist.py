ae09e056a5aafcce3e229ddeeadae306113cbbd1
Austin Wang <austinwang@Austins-MBP>
2021-01-22 09:47:33 -0800

    expression histogram

---
+++ visualize/vis_expression_dist.py
@@ -0,0 +1,13 @@
+import scanpy as sc
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+data_path = "/dfs/user/atwang/data/spt_zhuang/source/processed_data/counts.h5ad"
+result_path = "/dfs/user/atwang/results/st_gnn_results/spt_zhuang/expression/hist.svg"
+
+adata = sc.read(data_path)
+sns.set()
+sns.distplot(adata.obs["total_counts"], kde=False, ax=axs[0])
+plt.savefig(result_path, bbox_inches='tight')
+plt.clf()
