8f9b0748fce08a92b82fda79f36784f73f488014
Austin Wang <austinwang@Austins-MBP>
2021-01-22 10:00:23 -0800

    expression histogram

--- visualize/vis_expression_dist.py
+++ visualize/vis_expression_dist.py
@@ -1,3 +1,4 @@
+import os
 import scanpy as sc
 import matplotlib
 matplotlib.use('Agg')
@@ -5,10 +6,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data_path = "/dfs/user/atwang/data/spt_zhuang/source/processed_data/counts.h5ad"
-result_path = "/dfs/user/atwang/results/st_gnn_results/spt_zhuang/expression/hist.svg"
+result_dir = "/dfs/user/atwang/results/st_gnn_results/spt_zhuang/expression"
+os.makedirs(result_dir, exist_ok=True)
 
 adata = sc.read(data_path)
+sc.pp.calculate_qc_metrics(adata)
+sc.pp.normalize_total(adata, target_sum=1e4)
+sc.pp.log1p(adata)
+sc.pl.highly_variable_genes(adata)
+plt.savefig(os.path.join(result_dir, "var_genes.svg"), bbox_inches='tight')
+plt.clf()
+
 sns.set()
 sns.distplot(adata.obs["total_counts"], kde=False, ax=axs[0])
-plt.savefig(result_path, bbox_inches='tight')
+plt.savefig(os.path.join(result_dir, "hist.svg"), bbox_inches='tight')
 plt.clf()
