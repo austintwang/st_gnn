d3be59591da834cd012f3beba3f242d9f8fbb4d1
Austin Wang <austinwang@Austins-MBP>
2021-02-07 17:54:58 -0800

    data loader

---
+++ model/loaders.py
@@ -0,0 +1,123 @@
+import os
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import torch
+from torch_geometric.data import Data, GraphSAINTRandomWalkSampler
+
+class Loader(object):
+    def __init__(self, **kwargs):
+        self.params = kwargs
+
+        in_data, partitions = self._import_data()
+        self.train_part, self.val_part, self.test_part = partitions
+
+        self.train_data, self.train_maps = self._build_graph(in_data, self.train_part)
+        self.val_data, self.val_maps = self._build_graph(in_data, self.val_part)
+        self.test_data, self.test_maps = self._build_graph(in_data, self.test_part)
+
+        self.train_sampler = self._build_sampler()
+
+    # def _import_data(self):
+    #     pass
+
+    # def _build_graph(self, in_data, partition):
+    #     pass
+
+    # def _build_sampler(self):
+    #     pass
+
+class SaintRWLoader(Loader):
+    def _build_sampler(self):
+        sampler = GraphSAINTRandomWalkSampler(
+            self.train_data, 
+            batch_size=self.params["batch_size"],
+            walk_length=self.params["saint_walk_length"],
+            num_steps=self.params["saint_num_steps"], 
+            sample_coverage=self.params["saint_sample_coverage"],
+        )
+        return sampler
+
+class ZhuangBasic(SaintRWLoader):
+    def _import_data(self):
+        anndata = self.params.get("st_anndata", sc.read(self.params["st_exp_path"]))
+        coords = self.params.get("st_coords", pd.read_pickle(self.params["st_coords_path"]))
+        organisms = self.params.get("st_organisms", pd.read_pickle(self.params["st_organisms_path"]))
+
+        m1 = random.shuffle(organisms["mouse1"])
+        num_train = int(0.8 * self.params["train_prop"])
+        train = m1[:num_train]
+        val = m1[num_train:]
+        test = organisms["mouse2"]
+
+        in_data = (anndata, coords)
+        partitions = (train, val, test)
+        return in_data, partitions
+
+    def _build_graph(self, in_data, partition):
+        st_anndata, st_coords = in_data
+
+        genes = np.array(st_anndata.var.var_names)
+        gene_to_node = {val: ind for ind, val in enumerate(genes)}
+        num_genes = len(genes)
+
+        cells = np.fromiter(i for i in st_anndata.obs.obs_names if i in partition)
+        cell_to_node = {val: ind + num_genes for ind, val in enumerate(cells)}
+        num_cells = len(cells)
+        coords = torch.tensor([st_coords[i] for i in cells])
+
+        x = torch.zeros(num_genes + num_cells, num_genes + 1)
+        x[:num_genes,:num_genes].fill_diagonal_(1.)
+        x[num_genes:,-1].fill_(1.)
+        node_to_id = np.concatenate((genes, cells))
+
+        expr = st_anndata.X
+        edges_l = []
+        edge_features_l = []
+        threshold = self.params["st_exp_threshold"]
+        for index, x in expr:
+            x = log(x)
+            if x >= threshold:
+                cell, gene = index
+                a = cell + num_genes
+                b = gene
+                edges_l.append([a, b])
+                edges_l.append([b, a])
+                edge_features_l.append(x)
+                edge_features_l.append(x)
+
+        edges = torch.tensor(edges_l).transpose_(0, 1)
+        edge_features = torch.tensor(edge_features_l)
+
+        data = Data(x=x, edge_index=edges, edge_attr=edge_features, y=coords)
+        maps = {
+            "gene_to_node": gene_to_node,
+            "cell_to_node": cell_to_node,
+            "node_to_id": node_to_id,
+        }
+
+        return data, maps
+
+
+if __name__ == '__main__':
+    data_path = "/dfs/user/atwang/data/spt_zhuang/"
+    coords_path = os.path.join(data_path, "parsed", "cell_coords.pickle")
+    orgs_path = os.path.join(data_path, "parsed", "cell_orgs.pickle")
+    exp_path = "/dfs/user/atwang/data/spt_zhuang/source/processed_data/counts.h5ad"
+
+    params = {
+        "batch_size": 500,
+        "saint_walk_length": 2,
+        "saint_num_steps": 5,
+        "saint_sample_coverage": 100,
+        "st_exp_path": exp_path,
+        "st_coords_path": coords_path,
+        "st_organisms_path": orgs_path,
+        "train_prop": 0.8,
+        "st_exp_threshold": 0.001,
+
+    }
+    loader = ZhuangBasic(**params)
+    for i in loader.train_sampler:
+        print(i)
+
---
+++ process/build_coords.py
@@ -0,0 +1,82 @@
+import os
+import glob
+import numpy as np 
+import snap
+import tqdm
+import pickle
+
+def parse_cell(line):
+    entries = line.split("\"")
+    if len(entries) == 1:
+        cell_id, x, y, slice_id = line.split(",")
+        x = float(x)
+        y = float(y)
+        z = int(slice_id.split("_")[1][5:]) * 10
+        # print(x, y) ####
+        return cell_id, x, y, z
+        
+    cell_id, x_b, _, y_b, slice_id = line.split("\"")
+    cell_id = cell_id.rstrip(",")
+
+    x1 = np.fromstring(x_b, sep=",")
+    y1 = np.fromstring(y_b, sep=",")
+    x2 = np.roll(x1, -1)
+    y2 = np.roll(y1, -1)
+    q = x1 * y2 - x2 * y1
+    a = np.sum(q)
+    if a == 0:
+        x = np.mean((x1 + x2)) / 2
+        y = np.mean((y1 + y2)) / 2
+        # print(x1, y1) ####
+    else:
+        x = np.sum((x1 + x2) * q) / (3 * a)
+        y = np.sum((y1 + y2) * q) / (3 * a)
+
+    z = int(slice_id.split("_")[1][5:]) * 10
+
+    return cell_id, x, y, z
+
+def load_file(coords, cells, in_path):
+    with open(in_path, "rb") as f:
+        lines = 0
+        buf_size = 1024 * 1024
+        buf = f.raw.read(buf_size)
+        while buf:
+            lines += buf.count(b'\n')
+            buf = f.raw.read(buf_size)
+
+    with open(in_path) as f:
+        next(f)
+        for line in tqdm.tqdm(f, desc=os.path.basename(in_path), total=lines):
+            cell_id, x, y, z = parse_cell(line)
+            coords[cell_id] = [x, y, z]
+            cells.append(cell_id)
+
+def build_coords(in_paths, out_dir):
+    coords = {}
+    cells = {}
+    for org in in_paths:
+        cells_org = cells.setdefault(org, [])
+        for p in org:
+            load_file(coords, cells_org, p)
+
+    out_path_coords = os.path.join(out_dir, "cell_coords.pickle")
+    out_path_coords = os.path.join(out_dir, "cell_orgs.pickle")
+    with open(out_path_coords, "wb") as out_file:
+        pickle.dump(coords)
+    with open(out_path_cells, "wb") as out_file:
+        pickle.dump(cells)
+
+if __name__ == '__main__':
+
+    data_path = "/dfs/user/atwang/data/spt_zhuang/"
+    in_dir = os.path.join(data_path, "source", "processed_data")
+    out_dir = os.path.join(data_path, "parsed")
+
+    indiv = ["mouse1", "mouse2"]
+    in_paths = {}
+    for i in indiv:
+        in_paths[i] = glob.glob(os.path.join(in_dir, f"segmented_cells_{i}sample*.csv"))
+    
+    build_coords(in_paths, out_dir)
+
