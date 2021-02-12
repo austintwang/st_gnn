47d26156032c8fd20d153be13b57cc2c7b1ee4f8
Austin Wang <austinwang@Austins-MBP>
2021-02-12 10:37:11 -0800

    gnn models

--- model/models.py
+++ model/models.py
@@ -15,7 +15,7 @@ class WRGCNConv(MessagePassing):
         self.root = Parameter(torch.Tensor(in_channels, out_channels))
         self.bias = Parameter(torch.Tensor(out_channels))
 
-    def forward(x, edge_index, edge_weight, edge_type):
+    def forward(self, x, edge_index, edge_weight, edge_type):
         num_nodes = x.size(0)
 
         out += x @ self.root + self.bias
