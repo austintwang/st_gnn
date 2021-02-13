fb03dbb2f28927061fafe0fe449d7fecefc3e757
Austin Wang <austinwang@Austins-MBP>
2021-02-12 17:53:29 -0800

    gnn models

--- model/trainers.py
+++ model/trainers.py
@@ -50,7 +50,7 @@ class Trainer(object):
         self.optimizer.zero_grad()
         batches = self.loader.train_sampler
         batch_records = {}
-        t_iter = tqdm.tqdm(batches, desc="\tLoss: ------", width=10)
+        t_iter = tqdm.tqdm(batches, desc="\tLoss: ------")
         time_start = time.time() - self.time_ref
 
         for data in t_iter:
