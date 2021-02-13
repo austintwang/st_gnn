5e01d6f5bccfb61d841cb6320e2287cc6e35fbbc
Austin Wang <austinwang@Austins-MBP>
2021-02-12 16:05:15 -0800

    gnn models

--- model/trainers.py
+++ model/trainers.py
@@ -171,7 +171,7 @@ class SupTrainer(Trainer):
         num_cells = l.shape[0]
         rtile = l.unsqueeze(0).expand(num_cells, -1, -1)
         ctile = l.unsqueeze(1).expand(-1, num_cells, -1)
-        ldists = torch.clamp((rtile - ctile).abs().log(), min=min_ldist)
+        ldists = torch.clamp(((rtile - ctile)**2).mean(dim=2).log(), min=min_ldist)
 
         print(means.shape, lvars.shape, ldists.shape) ####
         nll = ((means - ldists) / lvars.exp())**2 / 2 + lvars
