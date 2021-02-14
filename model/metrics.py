c275ad78b4b82d56e90b58a1208281350287a084
Austin Wang <austinwang@Austins-MBP>
2021-02-13 16:53:14 -0800

    gnn models

--- model/metrics.py
+++ model/metrics.py
@@ -16,9 +16,9 @@ def gaussian_nll(pred, data, params):
 
     return metric.item()
 
-def _get_ranks(x):
+def _get_ranks(x, device):
     tmp = x.argsort(dim=1)
-    ranks = torch.zeros_like(tmp)
+    ranks = torch.zeros_like(tmp, device=device)
     for i in range(x.shape[0]):
         ranks[i,tmp[i,:]] = torch.arange(len(x))
     return ranks
@@ -26,12 +26,13 @@ def _get_ranks(x):
 @torch.no_grad()
 def spearman(pred, data, params):
     min_dist = params["min_dist"]
+    device = params["device"]
 
     pdists = pred["dists"]
     means = pdists[:,:,0]
 
-    x_rank = _get_ranks(means)
-    y_rank = _get_ranks(data.ldists)
+    x_rank = _get_ranks(means, device)
+    y_rank = _get_ranks(data.ldists, device)
 
     n = num_cells
     upper = 6 * torch.sum((x_rank - y_rank).pow(2), dim=1)
