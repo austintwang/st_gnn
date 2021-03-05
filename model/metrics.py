f093c163ed04c5a292c18eae3a195101efb1cd13
Austin Wang <austinwang@Austins-MBP>
2021-03-05 00:06:17 -0800

    non-synth vae

--- model/metrics.py
+++ model/metrics.py
@@ -40,7 +40,7 @@ def mse(pred, data, params, lbound=0., ubound=np.inf):
 
     idx = (data.dists >= lbound) & (data.dists <= ubound)
     err = ((pdists - data.dists)**2)[idx]
-    print(err) ####
+    print(data.dists) ####
 
     metric = torch.mean(err)
 
