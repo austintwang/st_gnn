29df97627a14f608d498e243b5026d68c859aa9c
Austin Wang <austinwang@Austins-MBP>
2021-03-08 01:04:08 -0800

    vae visualization

--- visualize/vis_vae_cells.py
+++ visualize/vis_vae_cells.py
@@ -40,7 +40,7 @@ def load_loader(loader_cls, params, clusters_path, cells_per_cluster):
     loader_params["num_cells_per_cluster"] = cells_per_cluster
     loader_params["batch_size"] = 400
     loader_params["saint_num_steps"] = {"train": 500, "val": 500, "test": 500}
-    loader_params["clear_cache"] = True ####
+    # loader_params["clear_cache"] = True ####
 
     loader = loader_cls(**loader_params)
     return loader
@@ -48,6 +48,8 @@ def load_loader(loader_cls, params, clusters_path, cells_per_cluster):
 def sample_model(loader, vae_model, num_select, num_total, device, mode):
     data_lst = []
     count = 0
+
+    print(len(loader.val_part), len(loader.test_part))
 
     if mode == "val":
         sampler = loader.val_sampler
