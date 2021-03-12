2fa300e31bb8404b0b6d0297cfb7c12d605dce66
Austin Wang <austinwang@Austins-MBP>
2021-03-11 23:10:25 -0800

    model ablation

--- model/dispatchers.py
+++ model/dispatchers.py
@@ -196,7 +196,7 @@ synth_vae_train_params = {
     "vae_exp_nll_w": 1.,
     "vae_sup_nll_w": 0.,
     "vae_struct_kl_w": 0.001,
-    "vae_exp_kl_w": 0.1,
+    "vae_exp_kl_w": 0.01,
 }
 
 synth_vae_model_params = {
